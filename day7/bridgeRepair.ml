e649bad5f45544f34fcfb4be74503225c8af7c9f
Dracnor <antoine.domenech@gmail.com>
2024-12-07 15:52:09 +0100

    day7 part 2

--- day7/bridgeRepair.ml
+++ day7/bridgeRepair.ml
@@ -7,33 +7,35 @@ let input_file =
   else
     Sys.argv.(1)
 
-(** Returns an iterator of the lines.
-  * Each lines is a (test value, number list) pair *)
+(** Returns the list of the lines.
+  * Each lines is a (test value, number list) pair. *)
 let read_input () =
   let i_c = open_in input_file in
-  let get_equation () =
-    try 
+  let rec loop () =
+    try
       let line = input_line i_c in
       let [@warning "-8"] [left; right] = String.split_on_char ':' line in
       let test_value = int_of_string left in
       let numbers = String.split_on_char ' ' right 
                     |> List.tl (* right starts with a space, so "" in split *)
                     |> List.map int_of_string  in
-      Some ((test_value, numbers), () )
+      (test_value, numbers) :: (loop ())
     with 
-      End_of_file -> close_in i_c; None
+      End_of_file -> close_in i_c; []
   in
-  Seq.unfold (fun () -> get_equation ()) ()
+  loop ()
+
+
 
 
 (* Part 1 *)
 
-(* Explain here if necessary *)
+(* Just check each equation. Parsing was the hardest thing. *)
 
-(** Checks if an equation is solvable.
-    total is the sum of previous good calibrations.
+(** Checks if an equation is solvable according to part1 ruleset.
+    total is the sum of previous part1 good calibrations.
     Returns the new value of total. *)
-let solvable total (test_value, numbers) =
+let part1_solvable total (test_value, numbers) =
   
   (** Goes through numbers left to right.
     * acc is what has already been computed from the left *)
@@ -44,24 +46,41 @@ let solvable total (test_value, numbers) =
   in
   total + (if loop (List.hd numbers) (List.tl numbers) then test_value else 0)
 
-let part1 iterator =
-  Seq.fold_left solvable 0 iterator
+let part1 eq_list =
+  List.fold_left part1_solvable 0 eq_list
+
 
 
 
 (* Part 2 *)
 
-(* Explain (very smartly) here if necessary *)
+(* Same but with a new operator.
+   I could do both part at once, but I find it cleaner this way. *)
 
-let part2 input =
-  -666
+(** Concatenates two int *)
+let int_cat x y =
+  (string_of_int x) ^ (string_of_int y)
+  |> int_of_string
+
+
+(** Same as part1 but with part2 ruleset. *)
+let part2_solvable total (test_value, numbers) =
   
+  (** Same as in part1 *)
+  let rec loop acc = function
+    | _ when acc > test_value -> false
+    | [] -> acc = test_value
+    | h :: t -> loop (acc * h) t || loop (acc + h) t || loop (int_cat acc h) t
+  in
+  total + (if loop (List.hd numbers) (List.tl numbers) then test_value else 0)
+
+let part2 eq_list =
+  List.fold_left part2_solvable 0 eq_list  
   
+
 
 
 (** main *)
 let () =
-  let input = read_input () in
-  Printf.printf "%d\n%d\n" (part1 input) (part2 input)
-
-
+  let eq_list = read_input () in
+  Printf.printf "%d\n%d\n" (part1 eq_list) (part2 eq_list)
